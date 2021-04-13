1c02475344ae578937ccd7756a72a15020afadea
DanielaGC <danielagc676@gmail.com>
2021-04-13 12:31:35 -0300

    Added 'ping shards' alias

--- src/commands/misc/PingCommand.js
+++ src/commands/misc/PingCommand.js
@@ -1,4 +1,4 @@
-const { CommandListener, Emoji } = require('../../utils')
+const { CommandListener, Emoji, EmbedBuilder } = require('../../utils')
 module.exports = class PingCommand extends CommandListener {
   constructor() {
     super({
@@ -14,6 +14,21 @@ module.exports = class PingCommand extends CommandListener {
   }
 
   run(ctx) {
-    ctx.reply('ping_pong', `**Pong!** \`${ctx.msg.channel.guild.shard.latency}ms\` (${Emoji.get('world').mention} Shard: ${ctx.msg.channel.guild.shard.id}/${ctx.client.shards.size})`)
+    switch (ctx.args[0]) {
+      case 'shards': {
+        const embed = new EmbedBuilder()
+        embed.setColor('DEFAULT')
+
+        for (const shard of ctx.client.shards) {
+          embed.addField(`Shard ${ctx.client.shards.get(shard[0]).id}`, `${Math.round(ctx.client.shards.get(shard[0]).latency)}ms`, true)
+        }
+        ctx.send(embed.build())
+      }
+        break;
+      default: {
+        ctx.reply('ping_pong', `**Pong!** \`${ctx.msg.channel.guild.shard.latency}ms\` (${Emoji.get('world').mention} Shard: ${ctx.msg.channel.guild.shard.id}/${ctx.client.shards.size})`)
+      }
+    }
+
   }
 }
