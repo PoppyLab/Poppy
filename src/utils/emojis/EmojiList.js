4bc844c88c0a1958f8cfc41fcf2f848e48518067
DanielaGC <danielagc676@gmail.com>
2021-04-14 00:24:25 -0300

    Update emoji list

--- src/utils/emojis/EmojiList.js
+++ src/utils/emojis/EmojiList.js
@@ -1,5 +1,7 @@
 module.exports = {
   world: '🌎',
   ping_pong: '🏓',
-  poppy_rip: '<:poppy_rip:830536105409511445>'
+  poppy_rip: '<:poppy_rip:830536105409511445>',
+  poppy_proud: '<:poppy_proud:830536249600245800>',
+  satellite: '📡'
 }
